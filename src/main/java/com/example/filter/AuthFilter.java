9d05aa60e01d21eaa2442a3e76696a0c1ba4ac51
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 22:12:21 +0500

    Update AuthFilter.java

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,8 +1,25 @@
 package com.example.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebFilter("/user/*")
 public class AuthFilter extends HttpFilter {
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest) req).getSession();
+        String user = (String) session.getAttribute("user");
+        if (user == null) {
+            req.getRequestDispatcher("/login.jsp").forward(req, res);
+        }
+        chain.doFilter(req, res);
+    }
     //write your code here!
 }
