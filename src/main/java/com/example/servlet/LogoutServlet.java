c39a2c8cdcafd7670aad2ce8719238cce75b44e8
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 22:17:11 +0500

    Update LogoutServlet.java

--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -1,7 +1,17 @@
 package com.example.servlet;
+
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/logout")
-public class LogoutServlet {
+public class LogoutServlet extends HttpServlet {
     //write your code here!
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        request.getSession().removeAttribute("user");
+        response.sendRedirect("/login.jsp");
+    }
 }
