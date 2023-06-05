14a632628f49ffa0d51e7ecf7adcd9d44986cb71
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 22:16:19 +0500

    Update LoginServlet.java

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,7 +1,39 @@
 package com.example.servlet;
+
 import javax.servlet.annotation.WebServlet;
+import com.example.Users;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet("/login")
-public class LoginServlet {
+public class LoginServlet extends HttpServlet {
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        HttpSession session = request.getSession();
+        String user = (String) session.getAttribute("user");
+        if (user == null) {
+            response.sendRedirect("/login.jsp");
+        } else {
+            response.sendRedirect("/user/hello.jsp");
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        if (Users.getInstance().getUsers().contains(login)
+                && !password.isEmpty()) {
+            request.getSession().setAttribute("user", login);
+            response.sendRedirect("/user/hello.jsp");
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+    }
     //write your code here!
 }
